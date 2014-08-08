ab41a2576045275a838edfab4ea593c9c176583a
Andreas Hocevar <andreas.hocevar@gmail.com>
2014-08-08 18:18:36 +0200

    Use local storage
    Initialize sthe todo list with example entries when there is no list in the
    local storage yet.

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -8,57 +8,7 @@
  */
 
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage, filterFilter) {
-	//var todos = $scope.todos = todoStorage.get();
-	var todos = $scope.todos = [
-	{
-		title: "ShopYEG.com: Complete the look and feel of the site and build the spinner in HTML/PHP",
-		dueDate: "1/8/2014",
-		statusFilter: "inactive",
-		completed: true
-	},
-	{
-		title: "Crystal Glass: Add new video to the home page",
-		dueDate: "2/25/2014",
-		statusFilter: "inactive",
-		completed: true
-	},
-	{
-		title: "Cleantech: Build the site in Wordpress",
-		dueDate: "3/15/2014",
-		statusFilter: "active",
-		completed: false
-	},
-	{
-		title: "The Bay Company: Build the site in Drupal 7",
-		dueDate: "6/5/2014",
-		statusFilter: "active",
-		completed: false
-	},
-	{
-		title: "The Victoria Foundation: Add toolbar buttons to TinyMCE in Drupal 7",
-		dueDate: "8/28/2014",
-		statusFilter: "inactive",
-		completed: true
-	},
-	{
-		title: "AIHA: Learn Python for remote contract work",
-		dueDate: "7/21/2014",
-		statusFilter: "active",
-		completed: false
-	},
-	{
-		title: "Crest.ca: Add pages based on client email",
-		dueDate: "8/28/2014",
-		statusFilter: "inactive",
-		completed: true
-	},
-	{
-		title: "Fort Air: Connect to Envista SQL backend through GoDaddy using ASPX and dump the minute-by-minute and hourly data into SQL backend on new server using PHP. Connect data to custom PHP calls to display information in HTML5 canvas object and allow user to manipulate time, number of stations displayed, number of substances displayed, and objective/guideline values.",
-		dueDate: "5/15/2014",
-		statusFilter: "active",
-		completed: false
-	}
-	];
+	var todos = $scope.todos = todoStorage.get();
 
 	$scope.predicate = 'dueDate';
 
--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -4,15 +4,57 @@
 /**
  * Services that persists and retrieves TODOs from localStorage
  */
-todomvc.factory('todoStorage', function () {
+todomvc.factory('todoStorage', function() {
 	var STORAGE_ID = 'todos-angularjs';
 
+	var initialTodos = [{
+		title: "ShopYEG.com: Complete the look and feel of the site and build the spinner in HTML/PHP",
+		dueDate: "1/8/2014",
+		statusFilter: "inactive",
+		completed: true
+	}, {
+		title: "Crystal Glass: Add new video to the home page",
+		dueDate: "2/25/2014",
+		statusFilter: "inactive",
+		completed: true
+	}, {
+		title: "Cleantech: Build the site in Wordpress",
+		dueDate: "3/15/2014",
+		statusFilter: "active",
+		completed: false
+	}, {
+		title: "The Bay Company: Build the site in Drupal 7",
+		dueDate: "6/5/2014",
+		statusFilter: "active",
+		completed: false
+	}, {
+		title: "The Victoria Foundation: Add toolbar buttons to TinyMCE in Drupal 7",
+		dueDate: "8/28/2014",
+		statusFilter: "inactive",
+		completed: true
+	}, {
+		title: "AIHA: Learn Python for remote contract work",
+		dueDate: "7/21/2014",
+		statusFilter: "active",
+		completed: false
+	}, {
+		title: "Crest.ca: Add pages based on client email",
+		dueDate: "8/28/2014",
+		statusFilter: "inactive",
+		completed: true
+	}, {
+		title: "Fort Air: Connect to Envista SQL backend through GoDaddy using ASPX and dump the minute-by-minute and hourly data into SQL backend on new server using PHP. Connect data to custom PHP calls to display information in HTML5 canvas object and allow user to manipulate time, number of stations displayed, number of substances displayed, and objective/guideline values.",
+		dueDate: "5/15/2014",
+		statusFilter: "active",
+		completed: false
+	}];
+
 	return {
-		get: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+		get: function() {
+			return JSON.parse(localStorage.getItem(STORAGE_ID)) || initialTodos;
 		},
 
-		put: function (todos) {
+		put: function(todos) {
 			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
 		}
 	};
