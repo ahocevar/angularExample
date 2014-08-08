a8a6515d790d2b731b1fa78b367385e7b474d109
RobertDenis <robdenis@shaw.ca>
2014-08-08 16:21:01 +0200

    Committing all files to repository
    This is the first, full commit of all files to the repository.

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -6,17 +6,72 @@
  * - retrieves and persists the model via the todoStorage service
  * - exposes the model to the template and provides event handlers
  */
+
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage, filterFilter) {
-	var todos = $scope.todos = todoStorage.get();
+	//var todos = $scope.todos = todoStorage.get();
+	var todos = $scope.todos = [
+	{
+		title: "ShopYEG.com: Complete the look and feel of the site and build the spinner in HTML/PHP",
+		dueDate: "1/8/2014",
+		statusFilter: "inactive",
+		completed: true
+	},
+	{
+		title: "Crystal Glass: Add new video to the home page",
+		dueDate: "2/25/2014",
+		statusFilter: "inactive",
+		completed: true
+	},
+	{
+		title: "Cleantech: Build the site in Wordpress",
+		dueDate: "3/15/2014",
+		statusFilter: "active",
+		completed: false
+	},
+	{
+		title: "The Bay Company: Build the site in Drupal 7",
+		dueDate: "6/5/2014",
+		statusFilter: "active",
+		completed: false
+	},
+	{
+		title: "The Victoria Foundation: Add toolbar buttons to TinyMCE in Drupal 7",
+		dueDate: "8/28/2014",
+		statusFilter: "inactive",
+		completed: true
+	},
+	{
+		title: "AIHA: Learn Python for remote contract work",
+		dueDate: "7/21/2014",
+		statusFilter: "active",
+		completed: false
+	},
+	{
+		title: "Crest.ca: Add pages based on client email",
+		dueDate: "8/28/2014",
+		statusFilter: "inactive",
+		completed: true
+	},
+	{
+		title: "Fort Air: Connect to Envista SQL backend through GoDaddy using ASPX and dump the minute-by-minute and hourly data into SQL backend on new server using PHP. Connect data to custom PHP calls to display information in HTML5 canvas object and allow user to manipulate time, number of stations displayed, number of substances displayed, and objective/guideline values.",
+		dueDate: "5/15/2014",
+		statusFilter: "active",
+		completed: false
+	}
+	];
+
+	$scope.predicate = 'dueDate';
 
 	$scope.newTodo = '';
 	$scope.editedTodo = null;
 
-	$scope.$watch('todos', function () {
+	$scope.$watch('todos', function (newValue, oldValue) {
 		$scope.remainingCount = filterFilter(todos, { completed: false }).length;
 		$scope.completedCount = todos.length - $scope.remainingCount;
 		$scope.allChecked = !$scope.remainingCount;
-		todoStorage.put(todos);
+		if (newValue !== oldValue) { // This prevents unneeded calls to the local storage
+			todoStorage.put(todos);
+		}
 	}, true);
 
 	if ($location.path() === '') {
@@ -37,8 +92,16 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage,
 			return;
 		}
 
+		var today = new Date();
+		var month = today.getMonth() + 1;
+		var day = today.getDate();
+		var year = 1900 + today.getYear();
+		today = month + "/" + day + "/" + year;
+	
 		todos.push({
 			title: newTodo,
+			statusFilter: "active",
+			dueDate: today,
 			completed: false
 		});
 
